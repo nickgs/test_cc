c2f88edd44b2cb3d9e7a14ebcc2110a8069bccb5
nickgs <nickgs1@gmail.com>
2015-05-12 00:14:58 -0400

    Learning the flag library... added -card option with Mastercard support

--- test_cc.go
+++ test_cc.go
@@ -3,13 +3,30 @@ package main
 import(
   "fmt"
   "os"
+  "flag"
 )
 
 func main() {
-  card := VisaCard()
+  cardType := flag.String("card", "visa", "a credit card type - visa or mastercard accepted.");
+  card := "";
+  flag.Parse();
+
+  switch(*cardType) {
+    case "visa":
+      card = VisaCard();
+      break;
+    case "mastercard":
+      card = MasterCard();
+      break;
+  }
+
   fmt.Fprintln(os.Stdout, card);
 }
 
 func VisaCard() string {
   return "4111111111111111";
+}
+
+func MasterCard() string {
+  return "5105105105105100";
 }
--- test_cc_test.go
+++ test_cc_test.go
@@ -8,6 +8,14 @@ func TestVisaCard(t *testing.T) {
   card := VisaCard();
 
   if(card != "4111111111111111") {
-    t.Fatalf("VisaCard failed: %s", card)
+    t.Fatalf("VisaCard failed: %s", card);
+  }
+}
+
+func TestMasterCard(t *testing.T) {
+  card := MasterCard();
+
+  if(card != "5105105105105100") {
+    t.Fatalf("MasterCard failed: %s", card);
   }
 }
